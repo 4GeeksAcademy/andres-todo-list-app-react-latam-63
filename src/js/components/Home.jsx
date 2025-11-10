ddb204b0925e74cff8a340a9d4c40208aec836e8
AEGiner <andresesquivel2011@gmail.com>
2025-11-10 21:33:39 +0000

    Created Todo List

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,15 @@
 import React from "react";
+import List from "./List";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="d-flex justify-content-center row mt-5">
+				<h1 className="title col-12 text-center">todos</h1>
+				<List />
+			</div>
 		</div>
 	);
 };
---
+++ src/js/components/List.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+
+const List = () => {
+
+    const [tasks, setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [isVisible, setIsVisible] = useState(null)
+
+    const CreateTask = (event) => {
+        if (event.key === 'Enter' && inputValue !== '') {
+            setTasks([...tasks, inputValue]); setInputValue('')
+        }
+    };
+
+    const DeleteTask = (index) => {
+        const newTasks = tasks.filter((item, currentIndex) => currentIndex !== index);
+        setTasks(newTasks)
+    };
+
+    const Capitalize = (item) => {
+        return item.charAt(0).toUpperCase() + item.slice(1);
+    };
+
+    const TaskCounter = (array) => {
+        if (array.length === 1) {
+            return `${array.length} Item left`
+        } return `${array.length} Items left`
+    };
+
+    const MouseEnter = (event) => {
+        setIsVisible(event.target.id);
+    };
+
+    const MouseLeave = () => {
+        setIsVisible(null);
+    };
+
+    function Visibility(item) {
+        if (isVisible == item) {
+            return 'visible'
+        } return 'hidden'
+    };
+
+
+    return (
+        <ul className="todo-list list-group col-6 list-group-flush">
+            <li className="list-group-item ps-5"><input type="text" className="input-field" placeholder="What needs to be done?" onChange={e => setInputValue(e.target.value)} value={inputValue} onKeyDown={CreateTask} /></li>
+            {tasks.map((item, index) => {
+                return (
+                    <li className="list-group-item ps-5 list-item" key={index} id={index} onMouseEnter={MouseEnter} onMouseLeave={MouseLeave}>{Capitalize(item)}
+                    <i className="fa-solid fa-x delete-marker" key={index} style={{ visibility: `${Visibility(index)}` }} onClick={() => DeleteTask(index)}></i></li>
+                )
+            })}
+            <li className="task-counter list-group-item ps-3">{TaskCounter(tasks)}</li>
+        </ul>
+    )
+};
+export default List;
